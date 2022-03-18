84aa58d1ee7f13fe97da22ea3345d17df1721cdb
Manny <mannysth@yahoo.com>
2022-03-18 15:24:37 -0700

    first commit, passes all test

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-// Write your JavaScript here
+let amountDue = document.getElementById('amount-due');
+let amountGiven = document.getElementById('amount-received');
+let dollars = document.getElementById('dollars-output');
+let quarters = document.getElementById('quarters-output');
+let dimes = document.getElementById('dimes-output');
+let nickels = document.getElementById('nickels-output');
+let pennies = document.getElementById('pennies-output');
+
+const handleChangeButton = e => {
+    let diff = amountGiven.value - amountDue.value;
+    let change = amountGiven.value - amountDue.value;
+    if (diff > 0) {
+        dollars.innerHTML = Math.floor(change / 1);
+        change = (change % 1).toFixed(2);
+    }
+    if (diff > 0) {
+        quarters.innerHTML = Math.floor(change / 0.25);
+        change = (change % 0.25).toFixed(2);
+    }
+    if (diff > 0) {
+        dimes.innerHTML = Math.floor(change / 0.10);
+        change = (change % 0.10).toFixed(2);
+    }
+    if (diff > 0) {
+        nickels.innerHTML = Math.floor(change / 0.05);
+        change = (change % 0.05).toFixed(2);
+    }
+    if (diff > 0) {
+        pennies.innerHTML = Math.floor(change / 0.01);
+        change = (change % 0.01).toFixed(2);
+    }
+}
+
+document.getElementById('calculate-change').addEventListener('click', () => handleChangeButton());
